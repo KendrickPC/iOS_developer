be6922cbe55294987daa20208543898d6e737615
kendrickpc <kennethpchang@gmail.com>
2019-11-10 09:30:57 +0800

    first commit

---
+++ 0_swift_for_beginners/sandbox.playground/Contents.swift
@@ -0,0 +1,60 @@
+//import UIKit
+//
+//var str = "Hello, playground"
+
+//print("Hello from the iOS team")
+
+
+//var question = "Are you ready to write your first lines of Swift code?"
+//print(question)
+//
+//var response = "I think so"
+//print(response)
+
+//var greeting = "Hello world!"
+//print(greeting)
+
+// Consider a tomato soup recipe that makes 4 servings...
+//var numberOfServingsForRecipe = 4
+//var desiredNumberOfServings = 16
+//
+//var servingsFactor = desiredNumberOfServings/numberOfServingsForRecipe
+//
+//// Calculate the number of pounds of tomatoes needed for 8 servings.
+//var poundsOfTomatoesForRecipe = 2
+//var amountToUseToday = poundsOfTomatoesForRecipe * servingsFactor
+//
+//print(amountToUseToday)
+
+//var petsAge = 12
+//var myMiddleInitial: Character = "A"
+//var numberOfWheels: Int = 4
+//var centimetersOfRainfall: Float = 5.5
+//var pi: Double = 3.14159265359
+//var letterOfTheDay: Character = "z"
+//var myFavoriteAnimal: String  = "nudibranch"
+//var rainingOutside: Bool  = true
+
+
+//{nameOfVariable} = {newValue}
+//var petsAge = 13
+//var petsAge = 14
+//
+//print(petsAge)
+
+// Example 1:
+let encouragement = "We can do it!"
+var customizedEncouragement = "You can do it Ken Dog!"
+customizedEncouragement = "You can do it Kenderson!"
+
+// Example 2:
+let birthYear = 1982
+var currentYear = 2018
+var age = currentYear - birthYear
+currentYear = 2019
+age = currentYear - birthYear
+
+print(age)
+
+//let constantString = "The world is flat."
+//constantString = "The world is round"
