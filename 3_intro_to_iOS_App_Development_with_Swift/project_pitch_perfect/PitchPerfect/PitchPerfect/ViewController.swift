6d73c837b0a6adf8b438af86b510f2178e310729
kendrickpc <kennethpchang@gmail.com>
2019-12-20 18:17:55 +0900

    IBAction and IBOutlet buttons added.

--- 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
+++ 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  PitchPerfect
 //
-//  Created by Kenneth Chang on 12/20/19.
+//  Created by Kenneth P. Chang on 12/20/19.
 //  Copyright © 2019 Udacity. All rights reserved.
 //
 
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed.")
+        recordingLabel.text = "Recording in progress"
+    }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording buttonw as pressed.")
+    }
+        
 }
 
