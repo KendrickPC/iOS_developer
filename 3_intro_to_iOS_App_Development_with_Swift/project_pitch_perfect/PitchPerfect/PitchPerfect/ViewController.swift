77a33ac2616c431de5e47f1a8485de00b8b4f43a
kendrickpc <kennethpchang@gmail.com>
2019-12-27 17:51:19 +0800

    Created Hello World in Swift.

---
+++ 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Kenneth Chang on 12/27/19.
+//  Copyright © 2019 Kenneth P. Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
