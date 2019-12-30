ffe0f66fa0b02c7d5c52ef09accdd02b1d5717b1
kendrickpc <kennethpchang@gmail.com>
2019-12-30 16:46:28 +0800

    override func didReceiveMemoryWarning added to view controller

--- 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
+++ 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
@@ -14,7 +14,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
