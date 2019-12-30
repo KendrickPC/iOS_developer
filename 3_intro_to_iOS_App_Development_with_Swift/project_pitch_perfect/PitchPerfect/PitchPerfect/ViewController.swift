8aaabf2b6da266bf6728e02c62dca68b1fd4b015
kendrickpc <kennethpchang@gmail.com>
2019-12-30 17:35:02 +0800

    Constraints integrated properly to two buttons and a label.

--- 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
+++ 3_intro_to_iOS_App_Development_with_Swift/project_pitch_perfect/PitchPerfect/PitchPerfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +21,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button was pressed")
+        recordingLabel.text = "Recording in Progress"
+    }
+
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording button was pressed.")
     }
 
 }
