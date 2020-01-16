88fcc644be043903f722cf883f1e390fd24cb9e0
kendrickpc <kennethpchang@gmail.com>
2020-01-16 17:22:56 +0800

    Click Counter app started.

---
+++ 4_UIKit_Fundamentals/lesson1_outlets_and_actions/Click Counter/Click Counter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Kenneth Chang on 1/16/20.
+//  Copyright © 2020 Kenneth P. Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text = "0"
+        print("viewDidLoad function called.")
+        view.addSubview(label)
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+    }
+}
+
