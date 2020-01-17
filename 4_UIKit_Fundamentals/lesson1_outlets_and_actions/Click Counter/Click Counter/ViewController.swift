32cd7d69cb3455312331f2f5ce5c085dec054fc1
kendrickpc <kennethpchang@gmail.com>
2020-01-17 17:15:37 +0800

    Story Board version of click counter working properly.

--- 4_UIKit_Fundamentals/lesson1_outlets_and_actions/Click Counter/Click Counter/ViewController.swift
+++ 4_UIKit_Fundamentals/lesson1_outlets_and_actions/Click Counter/Click Counter/ViewController.swift
@@ -11,59 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     var count = 0
-    var label: UILabel!
-    // Label #2: Add an additional label and have it increment with the first
-    var label_2: UILabel!
+    @IBOutlet var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // label
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        label.text = "label"
-        view.addSubview(label)
-        self.label = label
-        
-        // Label #2: Add an additional label and have it increment with the first
-        let label_2 = UILabel()
-        label_2.frame = CGRect(x: 150, y: 200, width: 60, height: 60)
-        label_2.text = "label_2"
-        view.addSubview(label_2)
-        self.label_2 = label_2
-
-        // button
-        let button = UIButton()
-        button.frame = CGRect(x: 150, y: 250, width: 150, height: 60)
-        button.setTitle("Increment Count", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
-        
-        // Add a decrement button
-        let decrement_button = UIButton()
-        decrement_button.frame = CGRect(x: 150, y: 350, width: 150, height: 60)
-        decrement_button.setTitle("Decrement Count", for: .normal)
-        decrement_button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(decrement_button)
-
-        // Callback method for incrementing count
-        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
-        // Callback method for decrementing count
-        decrement_button.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControl.Event.touchUpInside)
     }
     
     // Note: When assigning a button’s action in code, you will need to mark the function with @objc as we did in incrementCount().
-    @objc func incrementCount() {
+    @IBAction func incrementCount() {
         self.count += 1
         self.label.text = "\(self.count)"
-        self.label_2.text = "\(self.count)"
-    }
-    
-    // Add a decrement button
-    @objc func decrementCount() {
-        self.count -= 1
-        self.label.text = "\(self.count)"
-        self.label_2.text = "\(self.count)"
     }
     
 }
