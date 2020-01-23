e23f579eed947c59811aab67fd7ddc6e670a765b
kendrickpc <kennethpchang@gmail.com>
2020-01-23 14:05:51 +0800

    Color Changer with Sliders app completed and working properly.

--- 4_UIKit_Fundamentals/lesson1_outlets_and_actions/ColorPickerWithSliders/ColorPickerWithSliders/ViewController.swift
+++ 4_UIKit_Fundamentals/lesson1_outlets_and_actions/ColorPickerWithSliders/ColorPickerWithSliders/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabelOutlet: UILabel!
+    @IBOutlet weak var greenLabelOutlet: UILabel!
+    @IBOutlet weak var blueLabelOutlet: UILabel!
+
+    @IBOutlet weak var redSliderOutlet: UISlider!
+    @IBOutlet weak var greenSliderOutlet: UISlider!
+    @IBOutlet weak var blueSliderOutlet: UISlider!
+
+    @IBOutlet weak var displayingLabel: UILabel!
+    
+    var redColor : Float = 0
+    var greenColor : Float = 0
+    var blueColor : Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad function loaded.")
     }
 
-
+    @IBAction func redSliderAction(_ sender: UISlider) {
+        changeColors()
+    }
+    @IBAction func greenSliderAction(_ sender: UISlider) {
+        changeColors()
+    }
+    @IBAction func blueSliderAction(_ sender: UISlider) {
+        changeColors()
+    }
+    
+    func changeDisplayLabelColor() {
+        displayingLabel.backgroundColor = UIColor(red: CGFloat(redColor), green: CGFloat(greenColor), blue: CGFloat(blueColor), alpha: 1.0)
+        changeLabelNumber()
+    }
+    
+    func changeColors() {
+        redColor = redSliderOutlet.value
+        greenColor = greenSliderOutlet.value
+        blueColor = blueSliderOutlet.value
+        changeDisplayLabelColor()
+    }
+ 
+    func changeLabelNumber() {
+        let roundedRed = String(format: "%0.0f", (redColor * 255))
+        let roundedGreen = String(format: "%0.0f", (greenColor * 255))
+        let roundedBlue = String(format: "%0.0f", (blueColor * 255))
+        
+        redLabelOutlet.text = "Red: \(roundedRed)"
+        greenLabelOutlet.text = "Green: \(roundedGreen)"
+        blueLabelOutlet.text = "Blue: \(roundedBlue)"
+    }
+    
 }
 
--- 4_UIKit_Fundamentals/lesson1_outlets_and_actions/ColorPickerWithSliders/README.md
+++ 4_UIKit_Fundamentals/lesson1_outlets_and_actions/ColorPickerWithSliders/README.md
@@ -10,4 +10,13 @@
     - Control drag the stack into the view to add constraints. Add the first four constraints to the vertical stackview. 
     - Set each constraint constant to 0
     - With the stackview selected, choose fill for alignment and fill equally for distribution. Set spacing to 0.
+    
     - Drag a horizontal stackview into the vertical stack. Set alignment to fill and distribution to fill equally.
+    - Drag a label and a slider into the first horizontal stack view. Change the label color to red and change the slider colors.
+    - Drag a horizontal stackview into the vertical stack. Set alignment to fill and distribution to fill equally.
+    - Drag a label and a slider into the second horizontal stack view. Change the label color to green and change the slider colors.
+    - Drag a horizontal stackview into the vertical stack. Set alignment to fill and distribution to fill equally.
+    - Drag a label and a slider into the third horizontal stack view. Change the label color to blue and change the slider colors.
+
+    - Drag a fourth horizontal stack view under the vertical stack view.  
+
